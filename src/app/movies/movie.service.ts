6c25257344c01bc10d4c1b7fff801753ffdbdba7
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-02-15 21:07:09 +0200

    updated moviesservice and fetched data from api

--- src/app/movies/movie.service.ts
+++ src/app/movies/movie.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Movie } from '../models/movie';
+import { map, tap, pluck, toArray } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MovieService {
-  baseUrl: string = 'https://api.themoviedb.org/3/movie/'
-  apiKey: string = 'a32172eb02aa5a52453e624b530a9505'
+  baseUrl: string = 'https://api.themoviedb.org/3/';
+  apiKey: string = '&api_key=a32172eb02aa5a52453e624b530a9505';
+  popularMovies: string = 'discover/movie?sort_by=popularity.desc';
+  theatersDiscover: string = 'discover/movie?primary_release_date.gte=2018-07-15&primary_release_date.lte=2019-02-01'
+
+  tests;
 
   constructor(private http: HttpClient) { }
+
+  getPopularMovies(){
+    return this.http.get<Movie[]>(`${this.baseUrl}${this.popularMovies}${this.apiKey}`)
+        .pipe(pluck('results'))
+  }
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from './movie.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movies',
@@ -7,10 +9,23 @@ import { MovieService } from './movie.service';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
+  responses;
+  private destroy$ = new Subject()
 
   constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.movieService.getPopularMovies()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((movieData) => {
+        this.responses = movieData
+      })
   }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();  // trigger the unsubscribe
+    this.destroy$.complete(); // finalize & clean up the subject stream
+  }
+
 
 }

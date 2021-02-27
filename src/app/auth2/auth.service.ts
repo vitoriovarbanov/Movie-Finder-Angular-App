1c08c61e40fc2d103730dcc5db2e633e33f2bb91
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-02-27 13:53:30 +0200

    registered user not automatically signed in now

--- src/app/auth2/auth.service.ts
+++ src/app/auth2/auth.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AngularFireDatabase, AngularFireObject, AngularFireList } from '@angular/fire/database';
+import { flatMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,31 +14,37 @@ export class AuthService {
   id = new BehaviorSubject(null)
   newUserData: AngularFireList<Object>
   userData
+  newUser: boolean;
 
   constructor(public firebaseAuth: AngularFireAuth, private db: AngularFireDatabase,
     private router: Router, private http: HttpClient) {
-    this.firebaseAuth.authState.subscribe((user)=>{
-      if(user){
+    this.firebaseAuth.authState.subscribe((user) => {
+      if (user) {
         this.userData = user;
         this.signedin$.next(true)
-        localStorage.setItem('user', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user'));
-        this.getUserFirebase()
-        .subscribe((data) => {
-        const arr = Object.entries(data)
-        const userLocalStorage = JSON.parse(localStorage.getItem('user'))
-         if (userLocalStorage) {
-        const userEmail = userLocalStorage.email;
-        const userDatabaseMail = arr.find(x => x[1].email === userEmail)
-        let id = userDatabaseMail[0]
-        localStorage.setItem('firebaseId', id)
-        this.id.next(localStorage.getItem('firebaseId'))
+        if(this.newUser === true){
+          this.signedin$.next(false)
         }
-    })
-      }else{
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        this.getUserFirebase()
+          .subscribe((data) => {
+            const arr = Object.entries(data)
+            const userLocalStorage = JSON.parse(localStorage.getItem('user'))
+            if (userLocalStorage) {
+              const userEmail = userLocalStorage.email;
+              const userDatabaseMail = arr.find(x => x[1].email === userEmail)
+              if (!userDatabaseMail) {
+                this.signedin$.next(false)
+              } else {
+                let id = userDatabaseMail[0]
+                localStorage.setItem('firebaseId', id)
+                this.id.next(localStorage.getItem('firebaseId'))
+              }
+            }
+          })
+      } else {
         localStorage.setItem('user', null);
         this.signedin$.next(false)
-        JSON.parse(localStorage.getItem('user'));
       }
     })
   }
@@ -46,13 +53,14 @@ export class AuthService {
     this.firebaseAuth
       .createUserWithEmailAndPassword(email, password)
       .then(value => {
+        this.newUser = true;
         this.newUserData = this.db.list(`/users`) as AngularFireList<any>;
         this.newUserData.push({ email: email, favourites: ['no'] });
         this.router.navigate(['auth/login'])
         console.log('Success!', value);
       })
       .catch(err => {
-        console.log('Something went wrong:',err.message);
+        console.log('Something went wrong:', err.message);
       });
   }
 
@@ -61,11 +69,28 @@ export class AuthService {
       .signInWithEmailAndPassword(email, password)
       .then((result) => {
         this.signedin$.next(true)
+        this.newUser = false;
+        this.getUserFirebase()
+          .subscribe((data) => {
+            const arr = Object.entries(data)
+            const userLocalStorage = JSON.parse(localStorage.getItem('user'))
+            if (userLocalStorage) {
+              const userEmail = userLocalStorage.email;
+              const userDatabaseMail = arr.find(x => x[1].email === userEmail)
+              if (!userDatabaseMail) {
+                this.signedin$.next(false)
+              } else {
+                let id = userDatabaseMail[0]
+                localStorage.setItem('firebaseId', id)
+                this.id.next(localStorage.getItem('firebaseId'))
+              }
+            }
+          })
         this.router.navigate(['movies'])
         console.log('Nice, it worked!', result);
       })
       .catch(err => {
-        console.log('Something went wrong:',err.message);
+        console.log('Something went wrong:', err.message);
       });
   }
 
@@ -78,7 +103,7 @@ export class AuthService {
     })
   }
 
-  getUserFirebase(){
+  getUserFirebase() {
     return this.http.get(`https://movie-finder-angular-default-rtdb.firebaseio.com/users.json`)
   }
 

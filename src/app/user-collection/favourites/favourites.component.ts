a7d9ce8d354cb9531bd7b18427c0080583659831
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-02-28 11:33:41 +0200

    favourite movies component updates after removing movie from the list

--- src/app/user-collection/favourites/favourites.component.ts
+++ src/app/user-collection/favourites/favourites.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
-import { tap, flatMap, switchMap, map } from 'rxjs/operators';
-import { from } from 'rxjs';
+import { flatMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-favourites',
@@ -11,29 +10,39 @@ import { from } from 'rxjs';
 })
 export class FavouritesComponent implements OnInit {
   response
+  ChangeDetectorRef: any;
 
-  constructor(private route: ActivatedRoute, private userService: UserService) {}
+  constructor(private route: ActivatedRoute, private userService: UserService,
+    private zone: NgZone) {}
 
   ngOnInit(): void {
     this.userService.getUserFavourites(this.route.snapshot.params['id'])
       .subscribe(data=>{
         const filtered = Object.values(data)
         this.response = filtered.filter(x=>x!=='no')
-        //this.response = Object.values(data)
       })
   }
 
   removeMovie(movieId){
-    this.userService.getUserFavourites(this.route.snapshot.params['id'])
+    return this.userService.getUserFavourites(this.route.snapshot.params['id'])
       .pipe(map((data)=>{
         const removedMovie = Object.entries(data).find(x=>x[1].id===movieId)
         return removedMovie[0]
       }),flatMap((removedMovie)=>{
         console.log(removedMovie)
         return this.userService.removeMovie(removedMovie,this.route.snapshot.params['id'])
-      })).subscribe(data=>console.log(data))
+      })).subscribe(()=>this.zone.run(()=>{
+        console.log(this.zone)
+        this.reRenderMovies()
+      }))
   }
 
+  reRenderMovies(){
+    this.userService.getUserFavourites(this.route.snapshot.params['id'])
+      .subscribe(data=>{
+        this.response =  Object.values(data)
+      })
+  }
 
 
 }
